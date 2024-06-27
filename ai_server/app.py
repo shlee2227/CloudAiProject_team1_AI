9447efa5af20f42d80ffc124476f85a913a2ec34
EC2 Default User <ec2-user@ip-172-31-23-204.ap-northeast-2.compute.internal>
2024-06-27 15:41:26 +0000

    [EDIT] Disable Debug Mode

--- ai_server/app.py
+++ ai_server/app.py
@@ -65,4 +65,4 @@ def get_health_info():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500, debug=True)
+    app.run(host='0.0.0.0', port=5500)
